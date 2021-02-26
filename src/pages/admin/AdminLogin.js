ebbfad8f6fe5fffc4b13228be89760c2fa7aae4a
shrikant <shrikantdwivedi786@gmail.com>
2021-02-26 15:11:51 +0530

    login form admin

--- src/pages/admin/AdminLogin.js
+++ src/pages/admin/AdminLogin.js
@@ -1,6 +1,77 @@
+import React from 'react';
+import { Card, Container, Row , Col ,Form, Button,handleSubmit,validated} from 'react-bootstrap';
+
 function AdminLogin(){
+    const [validated, setValidated] = React.useState(false);
+
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+
+        setValidated(true);
+    };
     return(
-        <h1>Admin Login</h1>
+        <>
+     <Container style={{ marginTop: '30px' }} >
+    <Card style={{borderRadius:'30px 30px 30px 30px'}}>
+    <Card.Header style={{textAlign:'center',fontWeight:'700',borderRadius:'5px 5px 0 0px',fontSize:'1.6rem', backgroundColor:'#2980b9',color:'white',borderRadius:'30px 30px 0px 0px'}}>Admin Login </Card.Header>
+        <Card.Body style={{marginTop:'-20px',marginBottom: '-20px'}}>
+            <Row>
+                <Col md={5} className="log_img">
+                </Col>
+                <Col md={7} style={{marginLeft:'5px', padding:'112px',borderRadius:'10px'}}>
+                <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                                        <Form.Row>
+                                            <Form.Group as={Col} md="10" controlId="validationCustom01" >
+                                                <Form.Label>Login Id</Form.Label>
+                                                <Form.Control
+                                                    required
+                                                    type="text"
+                                                    placeholder="Enter Login Id"
+                                                    
+                                                />
+                                                <Form.Control.Feedback type="invalid">
+                                                   Enter Login Id
+                                                </Form.Control.Feedback>
+                                                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                            </Form.Group>
+                                        </Form.Row>
+
+                                        <Form.Row>
+                                            <Form.Group as={Col} md="10" controlId="validationCustom02">
+                                                <Form.Label>Password</Form.Label>
+                                                <Form.Control
+                                                    required
+                                                    type="password"
+                                                    placeholder="Enter Password"
+                                                />
+                                                <Form.Control.Feedback type="invalid">
+                                                Enter Password
+                                                </Form.Control.Feedback>
+                                                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                            </Form.Group>
+                                            </Form.Row>
+                                        
+                                        <center>
+                                        <Button style={{marginRight:'75px'}} variant="dark" size="lg">
+      Log In
+    </Button>
+                                        </center>
+                                        
+                                    
+ 
+                                    </Form>
+                                    
+                </Col>
+            </Row>
+        </Card.Body>
+    </Card>
+     </Container>
+<br></br>
+        </>
     )
 }
 
